adb26b8714cc002f71fcd024c7be7f1fcba7b3ff
alexk11 <akryuchkov1@gmail.com>
2026-04-25 22:38:52 +0300

    formatting (alt+ctrl+L)

--- event-manager/src/main/java/dev/eventmanager/security/SecurityConfig.java
+++ event-manager/src/main/java/dev/eventmanager/security/SecurityConfig.java
@@ -103,6 +103,7 @@ public class SecurityConfig {
                         "/v3/api-docs",
                         "/configuration/ui",
                         "/swagger-resources/**",
+                        "/configuration/security",
                         "/swagger-ui.html",
                         "/webjars/**",
                         "/v3/api-docs/swagger-config",
